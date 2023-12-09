1f62eedca6317da7b2a1927e6e31ee8b3be5565d
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-12-10 00:03:13 +0200

    done the third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector("form");
+const SAVE_MESSAGE = "feedback-form-state";
+const email = form.elements.email.value;
+const message = form.elements.message.value;
+
+form.addEventListener("submit", getFeedback)
+function getFeedback(event) {
+    event.preventDefault();
+    const jsonStr = localStorage.getItem(SAVE_MESSAGE);
+    if (jsonStr) {
+        const formData = JSON.parse(jsonStr);
+        console.log(formData);
+        localStorage.removeItem(SAVE_MESSAGE);
+        form.reset()
+    }
+};
+
+document.addEventListener("DOMContentLoaded", checkData);
+function checkData(event) {
+    const savedData = localStorage.getItem(SAVE_MESSAGE);
+
+    if (savedData) {
+        const savedDataForm = JSON.parse(savedData);
+        form.elements.email.value = savedDataForm.email;
+        form.elements.message.value = savedDataForm.message;
+    }
+}
+
+
+const saveData = () => {
+    const user = {
+        email: form.elements.email.value,
+        message: form.elements.message.value
+    };
+    const jsonThrottle = JSON.stringify(user);
+    localStorage.setItem(SAVE_MESSAGE,jsonThrottle)
+}
+const throttleData = throttle(saveData, 500);
+form.addEventListener('input', throttleData);
