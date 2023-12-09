137ad2b29ba0c31e13d4b712da8eb87259037359
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-12-09 22:35:48 +0200

    done second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe);
+const MEMORY_TIME = "videoplayer-current-time";
+const savedTime = localStorage.getItem(MEMORY_TIME) || 0;
+player.setCurrentTime(savedTime);
+const saveTime = function (data) {
+    localStorage.setItem(MEMORY_TIME, data.seconds);
+
+};
+const throttleTime = throttle(saveTime, 1000)
+
+player.on('timeupdate', throttleTime);
+
 
